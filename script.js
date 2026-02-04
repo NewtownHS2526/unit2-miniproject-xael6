f2b3c556055fe554a5313e0c05e606d122b42fbc
Xael Garcia <xaelg2@nycstudents.net>
2026-02-04 11:36:38 -0500

    done

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 // YOUR CODE HERE
 
-// getRandomInt() - gets a random number between 1 and 100
-// is_dark(hex) - takes in a hex number (for example #123456) and returns true if it's dark and false if it's light.
+// getRandomInt() - gets a random number between 1 and 100 
+function getRandomInt() {
+    return Math.floor(Math.random() * 100) + 1;
+} 
+// is_dark(hex) - takes in a hex number (for example rgb(81, 68, 153)) and returns true if it's dark and false if it's light. 
+const is_dark = (hexNum) => {
+  const rgb = hexNum
+    .replace("#", "")
+    .match(/.{1,2}/g)
+    .map((c) => parseInt(c, 16));
+  
+  console.log( rgb);
+  const average =  rgb.reduce((a, b) => a + b, 0) /  rgb.length;
+
+  return true ? average < 119 : false;
+};
